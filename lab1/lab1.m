51d94aa62840c5f2ca51ce148fa9efff6bac46ab
Laura Pérez Mayos <lpmayos@gmail.com>
2017-03-07 23:14:18 +0100

    better image caption

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -85,9 +85,9 @@ end
  
 I2Decomposed = apply_H(I, HDecomposed);
 fig = figure(2);
-subplot(1,3,1); imshow(I); title('original');
-subplot(1,3,2); imshow(I2); title('transformed with H');
-subplot(1,3,3); imshow(I2Decomposed); title('transform with H decomposed');
+subplot(1,3,1); imshow(I); title('Original image');
+subplot(1,3,2); imshow(I2); title('Affine transformation with H');
+subplot(1,3,3); imshow(I2Decomposed); title('Affine transformation with H decomposed');
 
 
 % %% 1.3 Projective transformations (homographies)
