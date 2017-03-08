0bce10ecd95159cb52995206bcf1c84eab5f95ff
cristinabustos16 <mcb9216@gmail.com>
2017-03-08 01:05:02 +0100

    merge solved

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -21,42 +21,16 @@ I=imread('Data/0005_s.png'); % we have to be in the proper folder
 % R is a rotation matrix (orthogonal matrix) and t is a translation vector.
 
 scalingFactor = 1;
-rotationAngle = 10 / 360 * 2 * pi;
-translation_i = 0;
-translation_j = 0;
+rotationAngle = 45 / 360 * 2 * pi;
+translationX = 0;
+translationY = 0;
 
-H=[ scalingFactor * cos(rotationAngle)   scalingFactor * -sin(rotationAngle)    translation_i;
-    scalingFactor * sin(rotationAngle)   scalingFactor *  cos(rotationAngle)    translation_j;
+H=[ scalingFactor * cos(rotationAngle)   scalingFactor * -sin(rotationAngle)    translationX;
+    scalingFactor * sin(rotationAngle)   scalingFactor *  cos(rotationAngle)    translationY;
     0                                    0                                      1];
 
 I2 = apply_H(I, H);
-figure; imshow(I);
-hold on
-l1 = [0, -1, 100]';
-t=1:0.1:1000;
-plot(t, -(l1(1)*t + l1(3)) / l1(2), 'y');
-x1 = 100;
-y1 = 200;
-x2 = 100;
-y2 = 400;
-plot(y1, x1, '*r', 'LineWidth', 3)
-plot(y2, x2, '*b', 'LineWidth', 3)
-x1r = H(2,:) * [y1, x1, 1]';
-y1r = H(1,:) * [y1, x1, 1]';
-x2r = H(2,:) * [y2, x2, 1]';
-y2r = H(1,:) * [y2, x2, 1]';
-plot(y1r, x1r, '*r', 'LineWidth', 3)
-plot(y2r, x2r, '*b', 'LineWidth', 3)
-
-figure; imshow(uint8(I2));
-hold on
-l1r = plot_transformed_line(l1, I, H);
-x1r = H(2,:) * [y1, x1, 1]';
-y1r = H(1,:) * [y1, x1, 1]';
-x2r = H(2,:) * [y2, x2, 1]';
-y2r = H(1,:) * [y2, x2, 1]';
-plot(y1r, x1r, '*r', 'LineWidth', 3)
-plot(y2r, x2r, '*b', 'LineWidth', 3)
+figure; imshow(I); figure; imshow(uint8(I2));
 
 
 %% 1.2. Affinities
@@ -172,13 +146,13 @@ l3 = cross(p5,p6);
 l4 = cross(p7,p8);
 
 % show the chosen lines in the image
-% figure;imshow(I);
-% hold on;
-% t=1:0.1:1000;
-% plot(t, -(l1(1)*t + l1(3)) / l1(2), 'r');
-% plot(t, -(l2(1)*t + l2(3)) / l2(2), 'g');
-% plot(t, -(l3(1)*t + l3(3)) / l3(2), 'b');
-% plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
+figure;imshow(I);
+hold on;
+t=1:0.1:1000;
+plot(t, -(l1(1)*t + l1(3)) / l1(2), 'r');
+plot(t, -(l2(1)*t + l2(3)) / l2(2), 'g');
+plot(t, -(l3(1)*t + l3(3)) / l3(2), 'b');
+plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
 
 % % ToDo: compute the homography that affinely rectifies the image
 
@@ -187,12 +161,13 @@ v1 = cross(l1, l2);
 v2 = cross(l3, l4);
 
 % compute the line at infinity
-linf = cross(v1, v2);
+l = cross(v1, v2);
 % normalize line at infinity
-linf = linf / max(linf);
+l = l / norm(l);
 
 % compute H based on the line at infinity, lecture2 slide 12
-H_p = [1 0 0; 0 1 0; linf'];
+H = [1 0 0; 0 1 0; l'];
+
 
 I2 = apply_H(permute(I,[2 1 3]), H);
 I2 = permute(I2,[2 1 3]);
@@ -201,41 +176,48 @@ figure; imshow(uint8(I2));
 % ToDo: compute the transformed lines lr1, lr2, lr3, lr4
 
 % Transpose of the inverse of H:
-H_p_inv_tras = (inv(H_p))';
+H_inv_tras = (inv(H))';
 
-l1_a = H_p_inv_tras * l1;
-l2_a = H_p_inv_tras * l2;
-l3_a = H_p_inv_tras * l3;
-l4_a = H_p_inv_tras * l4;
+lr1 = H_inv_tras * l1;
+lr2 = H_inv_tras * l2;
+lr3 = H_inv_tras * l3;
+lr4 = H_inv_tras * l4;
 
 % show the transformed lines in the transformed image
-% figure;imshow(uint8(I2));
-% hold on;
-% t=1:0.1:1000;
-% plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'r');
-% plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'g');
-% plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'b');
-% plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
-% l1_a = plot_transformed_line(l1, I, H_p);
-% l2_a = plot_transformed_line(l2, I, H_p);
-% l3_a = plot_transformed_line(l3, I, H_p);
-% l4_a = plot_transformed_line(l4, I, H_p);
+figure;imshow(uint8(I2));
+hold on;
+t=1:0.1:1000;
+plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'r');
+plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'g');
+plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'b');
+plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
 
 % ToDo: to evaluate the results, compute the angle between the different pair 
 % of lines before and after the image transformation
 
+% Lines before transformation in cartesian coordinates:
+l1c = [l1(1) / l1(3), l1(2) / l1(3)]';
+l2c = [l2(1) / l2(3), l2(2) / l2(3)]';
+l3c = [l3(1) / l3(3), l3(2) / l3(3)]';
+l4c = [l4(1) / l4(3), l4(2) / l4(3)]';
+
 % Angles of lines before transformation:
-ang_l1_l2_before = compute_angle(l1, l2);
-ang_l3_l4_before = compute_angle(l3, l4);
-fprintf('Angle of l1 and l2 before transformation = %f º\n', ang_l1_l2_before)
-fprintf('Angle of l3 and l4 before transformation = %f º\n', ang_l3_l4_before)
+ang_l1_l2_before = acos((l1c'*l2c)/sqrt((l1c'*l1c)*(l2c'*l2c)));
+ang_l3_l4_before = acos((l3c'*l4c)/sqrt((l3c'*l3c)*(l4c'*l4c)));
+fprintf('Angle of l1 and l2 before transformation = %f ?\n', ang_l1_l2_before)
+fprintf('Angle of l3 and l4 before transformation = %f ?\n', ang_l3_l4_before)
+
+% Lines after transformation in cartesian coordinates:
+l1rc = [lr1(1) / lr1(3), lr1(2) / lr1(3)]';
+l2rc = [lr2(1) / lr2(3), lr2(2) / lr2(3)]';
+l3rc = [lr3(1) / lr3(3), lr3(2) / lr3(3)]';
+l4rc = [lr4(1) / lr4(3), lr4(2) / lr4(3)]';
 
 % Angles of lines after transformation:
-ang_l1_l2_after = compute_angle(l1_a, l2_a);
-ang_l3_l4_after = compute_angle(l3_a, l4_a);
-fprintf('Angle of l1 and l2 after transformation = %f º\n', ang_l1_l2_after)
-fprintf('Angle of l3 and l4 after transformation = %f º\n', ang_l3_l4_after)
-
+ang_l1_l2_after = acos((lr1c'*lr2c)/sqrt((lr1c'*lr1c)*(lr2c'*lr2c)));
+ang_l3_l4_after = acos((lr3c'*lr4c)/sqrt((lr3c'*lr3c)*(lr4c'*lr4c)));
+fprintf('Angle of l1 and l2 after transformation = %f ?\n', ang_l1_l2_after)
+fprintf('Angle of l3 and l4 after transformation = %f ?\n', ang_l3_l4_after)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 3. Metric Rectification
