4a8832f90581c58def67da6c22955669856402dd
Laura Pérez Mayos <lpmayos@gmail.com>
2017-03-26 19:57:01 +0200

    task1

--- lab3/fundamental_matrix.m
+++ lab3/fundamental_matrix.m
@@ -12,7 +12,7 @@ for i=1:n
     u = x1_normalised(1,i);
     u_p = x2_normalised(1,i);
     v = x1_normalised(2,i);
-    v_p = x2_normalised(1,i);
+    v_p = x2_normalised(2,i);
     
     W(i, :) = [u*u_p, v*u_p, u_p, u*v_p, v*v_p, v_p, u, v, 1];
 end
@@ -20,7 +20,10 @@ end
 [U, D, V] = svd(W);
 
 f_rank3 = V(:,9);
-f_rank3 = reshape(f_rank3, [3,3]);
+% f_rank3 = reshape(f_rank3, [3,3]);  % TODO: review why reshape gives the transposed matrix ¿?
+f_rank3 = [f_rank3(1) f_rank3(2) f_rank3(3);
+           f_rank3(4) f_rank3(5) f_rank3(6);
+           f_rank3(7) f_rank3(8) f_rank3(9)];
 
 [U, D, V] = svd(f_rank3);
 
